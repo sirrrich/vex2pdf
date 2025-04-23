4329bc32c5e6da16c20a3647bfb99374d7ca03c3
Salem Bouraoui <jurassic.lizard@protonmail.com>
2025-04-23 11:11:49 +0200

    make advisory fields optional as per the cdx spec
       * mark advisory fields as optional to not fail when
    they do not exist

--- src/model/cyclonedx/non_root/advisory.rs
+++ src/model/cyclonedx/non_root/advisory.rs
@@ -1,6 +1,9 @@
 use super::*;
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Advisory {
-    pub title: String,
-    pub url: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url: Option<String>,
 }
